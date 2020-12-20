e984f6d7431838a96884041aa74540433b43d9f0
Max Mignolet <maxmignolet278@gmail.com>
2020-12-20 06:30:15 +0100

    few changes

--- weak/weak.py
+++ weak/weak.py
@@ -21,7 +21,7 @@ def modifFile(dt, dx, end_time, Du, Dv, f, k, r_treshold, sampling, param_file):
             file.write(str(param[i]) + '\n')
 
 dt = 1e-2
-dx_240 = 1e-1 # dx for 10 nodes and 24 threads/process
+dx_240 = 1e-5 # dx for 10 nodes and 24 threads/process, total time of ~3-4 min
 end_time = 1
 Du = 1e-5
 Dv = 4e-6
@@ -35,11 +35,11 @@ param_file = sys.argv[2]
 # nThreads = np.array([24, 20, 16, 12, 8, 4, 2, 1])
 nThreads = np.array([4, 2, 1])
 
-dx = dx_240 * np.sqrt(nThreads * nProcess / 240)
-print(dx)
+dx = dx_240 / np.sqrt(nThreads * nProcess / 240)
+
 for i in range(len(nThreads)):
         modifFile(dt, dx[i], end_time, Du, Dv, f, k, r_treshold, sampling, param_file)
-        run(["echo", "*** running with " + str(nThreads[i]) + " threads/process and dx = "  + str(dx[i]) + " ***"])
+        run(["echo", "*** running with " + str(nThreads[i]) + " threads/process, " + str(nProcess) + " processes and dx = "  + str(dx[i]) + " ***"])
         os.environ["OMP_NUM_THREADS"] = str(nThreads[i])
         run("./run.sh")
 print("Test finished wih success")
